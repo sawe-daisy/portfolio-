5b3774d315934f62375814f3b0279bf8f50dccdc
sawe-daisy <deesawe16@gmail.com>
2021-01-04 20:35:32 +0300

    about me component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Component1Component } from './component1/component1.component';
+import { Component2Component } from './component2/component2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Component1Component
+    Component1Component,
+    Component2Component
   ],
   imports: [
     BrowserModule,
